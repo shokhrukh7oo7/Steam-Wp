37702e9c6c02c6273490f4995d249351c4f4d22e
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-11-20 13:40:58 +0500

    about page fixed

--- template-parts/about-us.php
+++ template-parts/about-us.php
@@ -14,70 +14,61 @@ get_header();
                     <div class="col-12 col-xl-8 my-3">
                         <div class="about-left-wrapper">
                             <h1 class="about-left-header">
-                                С 2017 года мы развиваем тему велнеса и освещаем ценность
-                                саун в современном мире
+                                <?php the_field('about_top_header_title') ?>
                             </h1>
 
                             <div class="about-middle-content-wrapper">
-                                <h3 class="about-middle-header-title">Наша цель</h3>
+                                <h3 class="about-middle-header-title">
+                                    <?php the_field('about_title_under_header') ?>
+                                </h3>
                                 <p class="about-middle-content-description">
-                                    Предоставить полный ассортимент современных саун и
-                                    велнес-продуктов, которые способствуют расслаблению и
-                                    улучшают самочувствие. Мы стремимся создать комфорт и
-                                    восстановление, предлагая профессиональную помощь в выборе
-                                    услуг и обеспечивая отличное обслуживание клиентов.
+                                    <?php the_field('about_top_description') ?>
                                 </p>
                             </div>
 
                             <div class="about-bottom-content-wrapper">
                                 <h3 class="about-bottom-header-title">
-                                    Специализируясь исключительно на услугах сауны и велнеса,
-                                    вы получите:
+                                    <?php the_field('about_list_header') ?>
                                 </h3>
 
-                                <ul>
-                                    <li>
-                                        <img src="/assets/images/services/CheckCircle.png" alt="image" />
-                                        Прямое обслуживание от сертифицированных специалистов по
-                                        саунам
-                                    </li>
-                                    <li>
-                                        <img src="/assets/images/services/CheckCircle.png" alt="image" />
-                                        Бесплатные пробные сеансы в сауне
-                                    </li>
-                                    <li>
-                                        <img src="/assets/images/services/CheckCircle.png" alt="image" />
-                                        Гарантия на все пакеты услуг на 1 год
-                                    </li>
-                                    <li>
-                                        <img src="/assets/images/services/CheckCircle.png" alt="image" />
-                                        Обслуживание и поддержка с временем ответа от 1 дня
-                                    </li>
-                                    <li>
-                                        <img src="/assets/images/services/CheckCircle.png" alt="image" />
-                                        Более 1 000 велнес-продуктов и аксессуаров в наличии
-                                    </li>
-                                </ul>
+                                <?php if (have_rows('about_list')): ?>
+                                    <ul>
+                                        <?php while (have_rows('about_list')):
+                                            the_row();
+                                            $icon = get_sub_field('about_icon');
+                                            $text = get_sub_field('about_text');
+                                            ?>
+                                            <li>
+                                                <?php if ($icon): ?>
+                                                    <img src="<?php echo esc_url($icon); ?>" alt="<?php echo esc_attr($icon) ?>">
+                                                <? endif; ?>
+                                                <?php echo esc_html($text) ?>
+                                            </li>
+                                        <?php endwhile; ?>
+                                    </ul>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-12 col-xl-4 my-3">
-                        <div class="about-swiper-wrapper">
-                            <swiper-container class="mySwiper" navigation="true">
-                                <swiper-slide><img src="/assets/images/about-us/cecrtificate.png"
-                                        alt="image" /></swiper-slide>
-                                <swiper-slide><img src="/assets/images/about-us/cecrtificate.png"
-                                        alt="image" /></swiper-slide>
-                                <swiper-slide><img src="/assets/images/about-us/cecrtificate.png"
-                                        alt="image" /></swiper-slide>
-                                <swiper-slide><img src="/assets/images/about-us/cecrtificate.png"
-                                        alt="image" /></swiper-slide>
-                                <swiper-slide><img src="/assets/images/about-us/cecrtificate.png"
-                                        alt="image" /></swiper-slide>
-                            </swiper-container>
+
+                    <?php if (have_rows('certificates')): ?>
+                        <div class="col-12 col-xl-4 my-3">
+                            <div class="about-swiper-wrapper">
+                                <swiper-container class="mySwiper" navigation="true">
+                                    <?php while (have_rows('certificates')):
+                                        the_row();
+                                        $image = get_sub_field('certificate_image');
+                                        ?>
+                                        <swiper-slide>
+                                            <img src="<?php echo esc_url($image); ?>" alt="<?php echo esc_attr($image); ?>" />
+                                        </swiper-slide>
+                                    <?php endwhile; ?>
+                                </swiper-container>
+                            </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
+
                 </div>
             </div>
         </div>
@@ -88,73 +79,40 @@ get_header();
             <div class="about-us-card-container">
                 <div class="about-us-card-header-wrapper">
                     <h1 class="about-us-card-header-title">
-                        Предлагаем самые выгодные цены на услуги по ремонту и
-                        обслуживанию саун
+                        <?php the_field('about_header_title_bottom') ?>
                     </h1>
                     <p class="about-us-card-header-description">
-                        и заботимся о вашем комфорте.
+                        <?php the_field('about_description_title_bottom') ?>
                     </p>
                 </div>
 
                 <div class="about-us-card-wrapper">
                     <div class="row">
-                        <div class="col-12 col-md-6 col-xl-4 my-2">
-                            <div class="item">
-                                <img src="/assets/images/about-us/card-1.png" alt="image" />
-                                <h3 class="card-header-title">Обслуживание от 1 дня</h3>
-                                <p class="card-description">
-                                    Устраняем любые неисправности сауны. Обычно на это уходит
-                                    1-3 дня. Если ремонт сложный - предупредим заранее.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-xl-4 my-2">
-                            <div class="item">
-                                <img src="/assets/images/about-us/card-2.png" alt="image" />
-                                <h3 class="card-header-title">
-                                    Теплоизоляция для долгой службы
-                                </h3>
-                                <p class="card-description">
-                                    Мы улучшим теплоизоляцию вашей сауны с использованием
-                                    высококачественных материалов, чтобы поддерживать
-                                    оптимальную температуру и продлить срок эксплуатации.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-xl-4 my-2">
-                            <div class="item">
-                                <img src="/assets/images/about-us/card-3.png" alt="image" />
-                                <h3 class="card-header-title">
-                                    Ремонтируем только то, что сломалось
-                                </h3>
-                                <p class="card-description">
-                                    Не навязываем услуги. Диагностируем и согласовываем
-                                    стоимость заранее.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-xl-4 my-2">
-                            <div class="item">
-                                <img src="/assets/images/about-us/card-4.png" alt="image" />
-                                <h3 class="card-header-title">Гарантия на ремонт</h3>
-                                <p class="card-description">
-                                    Предоставляем гарантию 14 дней на все работы.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-xl-4 my-2">
-                            <div class="item">
-                                <img src="/assets/images/about-us/card-5.png" alt="image" />
-                                <h3 class="card-header-title">
-                                    Оригинальные комплектующие
-                                </h3>
-                                <p class="card-description">
-                                    Благодаря прямым связям с производителями, у нас всегда в
-                                    наличии необходимые оригинальные запчасти для ремонта
-                                    вашей сауны.
-                                </p>
-                            </div>
-                        </div>
+
+                        <?php if (have_rows('about_cards')): ?>
+                            <?php while (have_rows('about_cards')):
+                                the_row();
+                                $image = get_sub_field('card_image');
+                                $title = get_sub_field('card_header');
+                                $description = get_sub_field('card_description');
+                                ?>
+                                <div class="col-12 col-md-6 col-xl-4 my-2">
+                                    <div class="item">
+                                        <?php if ($image): ?>
+                                            <img src="<?php echo esc_url($image); ?>"
+                                                alt="<?php echo esc_attr($image); ?>" />
+                                        <?php endif; ?>
+                                        <?php if ($title): ?>
+                                            <h3 class="card-header-title"><?php echo esc_html($title); ?></h3>
+                                        <?php endif; ?>
+                                        <?php if ($description): ?>
+                                            <p class="card-description"><?php echo esc_html($description); ?></p>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+
                     </div>
                 </div>
             </div>
